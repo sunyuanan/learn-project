eb8e12b88f2fb0d9adc8d36e4ea3f2c60c6d83d3
SYA <2282324699@qq.com>
2023-04-19 16:44:40 +0800

    jwt+session  demo

---
+++ demo-jwt-session/src/main/java/com/example/demojwtsession/DemoJwtSessionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demojwtsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJwtSessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJwtSessionApplication.class, args);
+    }
+
+}
---
+++ demo-jwt-session/src/test/java/com/example/demojwtsession/DemoJwtSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demojwtsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJwtSessionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
